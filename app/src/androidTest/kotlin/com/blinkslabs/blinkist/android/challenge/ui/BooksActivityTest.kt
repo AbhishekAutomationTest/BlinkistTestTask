bbb10351bb40b9b89e31960e8a9e5f565e9a0ff1
Agrawal, Abhishek <abhishek.a32@gmail.com>
2021-06-11 23:46:30 +0700

    enable VCS & 1st Test Commit

---
+++ app/src/androidTest/kotlin/com/blinkslabs/blinkist/android/challenge/ui/BooksActivityTest.kt
@@ -0,0 +1,12 @@
+package com.blinkslabs.blinkist.android.challenge.ui
+
+import junit.framework.TestCase
+
+class BooksActivityTest : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    public override fun tearDown() {}
+}
